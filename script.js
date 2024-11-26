d7d755363eb66edb6e6875f8b0bce2c4e9be73d4
SethuMehansa <sethumisethumi@gmail.com>
2024-11-27 03:14:09 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const colapse=document.getElementById("collapse");
-colapse.forEach((colapses)=>{
-    colapses.addEventListener("click",()=>{
-        colapses.classList.toggle("active")
-    })
-})
+let length=0;
+let text="Here's a collection of fun, interactive websites I've built to explore creative coding and design.These projects are a mix of experimentation, learning."
+typing();
+function typing(){
+        if(length<text.length){
+        document.getElementById("typing").innerHTML+=text.charAt(length);
+        length++;
+        setTimeout(typing,)
+        
+    }
+}
